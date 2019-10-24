a50f7cbfccfe984205d5d07b534748051635cd90
Samed Demir <43873156+Sameetdmr@users.noreply.github.com>
2019-10-24 16:38:11 +0300

    login

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyCEA0J2R5pF3-dXU-cI00F7fhFR3f7P7gY",
+    authDomain: "project-6110f.firebaseapp.com",
+    databaseURL: "https://project-6110f.firebaseio.com",
+    projectId: "project-6110f",
+    storageBucket: "project-6110f.appspot.com",
+    messagingSenderId: "577692940584"
+};
+export default config;
